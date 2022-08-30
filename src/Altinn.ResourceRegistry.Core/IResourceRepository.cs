23eea817c21bdda23bd73dee33246e4098becc94
Rune T. Larsen <rune@hjemmekino.no>
2022-08-30 12:41:00 +0200

    Expaneded repository interface

--- src/Altinn.ResourceRegistry.Core/IResourceRepository.cs
+++ src/Altinn.ResourceRegistry.Core/IResourceRepository.cs
@@ -4,8 +4,12 @@ namespace Altinn.ResourceRegistry.Core
 {
     public interface IResourceRepository
     {
-
         ServiceResource GetResource(string id);
 
+        void DeleteResource(string id);
+
+        void UpdateResource(ServiceResource resource);
+
+        void CreateResource(ServiceResource resource);
     }
 }
--- src/Altinn.ResourceRegistry.Persistence/ResourceRepository.cs
+++ src/Altinn.ResourceRegistry.Persistence/ResourceRepository.cs
@@ -6,9 +6,24 @@ namespace Altinn.ResourceRegistry.Persistence
     public class ResourceRepository : IResourceRepository
 
     {
+        public void CreateResource(ServiceResource resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteResource(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public ServiceResource GetResource(string id)
         {
             return new ServiceResource() { identifier = "test_harcoded_resourcerepostory" };
+        }
+
+        public void UpdateResource(ServiceResource resource)
+        {
+            throw new NotImplementedException();
         }
     }
 }
