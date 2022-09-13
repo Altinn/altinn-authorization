96c2effb925d4af1bd3d23c691b1260bbddede72
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2022-09-13 11:20:12 +0200

    implemented resourceregistryrepository.CreateResource and DeleteResource

--- src/Altinn.ResourceRegistry.Persistence/ResourceRegistryRepository.cs
+++ src/Altinn.ResourceRegistry.Persistence/ResourceRegistryRepository.cs
@@ -5,6 +5,7 @@ using Altinn.ResourceRegistry.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Npgsql;
+using Npgsql.PostgresTypes;
 using System.Data;
 using System.Runtime.Serialization;
 using System.Text.Encodings.Web;
@@ -15,7 +16,9 @@ namespace Altinn.ResourceRegistry.Persistence
     {
         private readonly string _connectionString;
         private readonly ILogger _logger;
-        private readonly string getResourceById = "SELECT * from resourceregistrytest.get_resource_by_id(@_identifier)";
+        private readonly string getResource = "SELECT * FROM resourceregistry.get_resource(@_identifier)";
+        private readonly string createResource = "SELECT * FROM resourceregistry.create_resource(@_identifier, @_created, @_modified, @_serviceresourcejson)";
+        private readonly string deleteResource = "SELECT * FROM resourceregistry.delete_resource(@_identifier)";
 
         public ResourceRepository(
     IOptions<PostgreSQLSettings> postgresSettings,
@@ -27,29 +30,101 @@ namespace Altinn.ResourceRegistry.Persistence
                 postgresSettings.Value.AuthorizationDbPwd);
         }
 
-        public Task<List<ServiceResource>> Search(ResourceSearch resourceSearch)
+        public async Task<List<ServiceResource>> Search(ResourceSearch resourceSearch)
         {
             throw new NotImplementedException();
+            try
+            {
+                await using NpgsqlConnection conn = new NpgsqlConnection(_connectionString);
+                await conn.OpenAsync();
+
+                NpgsqlCommand pgcom = new NpgsqlCommand(getResource, conn);
+                pgcom.Parameters.AddWithValue("_searchTerm", resourceSearch.SearchTerm);
+
+                List<ServiceResource> serviceResources = new List<ServiceResource>();
+
+                using NpgsqlDataReader reader = pgcom.ExecuteReader();
+                if (reader.Read())
+                {
+                    serviceResources.Add(getServiceResource(reader));
+                }
+
+                return serviceResources;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Authorization // ResourceRegistryRepository // Search // Exception");
+                throw;
+            }
         }
 
-        Task IResourceRegistryRepository.CreateResource(ServiceResource resource)
+        public async Task<ServiceResource> CreateResource(ServiceResource resource)
         {
-            throw new NotImplementedException();
+            var json = System.Text.Json.JsonSerializer.Serialize(resource, new System.Text.Json.JsonSerializerOptions() { PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase });
+            try
+            {
+                await using NpgsqlConnection conn = new NpgsqlConnection(_connectionString);
+                await conn.OpenAsync();
+
+                NpgsqlCommand pgcom = new NpgsqlCommand(createResource, conn);
+                pgcom.Parameters.AddWithValue("_identifier", resource.Identifier);
+                pgcom.Parameters.AddWithValue("_created", NpgsqlTypes.NpgsqlDbType.TimestampTz ,DateTime.UtcNow);
+                pgcom.Parameters.AddWithValue("_modified", NpgsqlTypes.NpgsqlDbType.TimestampTz, DateTime.UtcNow);
+                pgcom.Parameters.AddWithValue("_serviceresourcejson", NpgsqlTypes.NpgsqlDbType.Jsonb, json);
+
+
+                using NpgsqlDataReader reader = pgcom.ExecuteReader();
+                if (reader.Read())
+                {
+                    return getServiceResource(reader);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("duplicate key value violates unique constraint"))
+                {
+                    return new ServiceResource();
+                }
+                _logger.LogError(e, "Authorization // ResourceRegistryRepository // GetResource // Exception");
+                throw;
+            }
         }
 
-        Task IResourceRegistryRepository.DeleteResource(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        async Task<ServiceResource> IResourceRegistryRepository.GetResource(string id)
+        public async Task<ServiceResource> DeleteResource(string id)
         {
             try
             {
                 await using NpgsqlConnection conn = new NpgsqlConnection(_connectionString);
                 await conn.OpenAsync();
 
-                NpgsqlCommand pgcom = new NpgsqlCommand(getResourceById, conn);
+                NpgsqlCommand pgcom = new NpgsqlCommand(deleteResource, conn);
+                pgcom.Parameters.AddWithValue("_identifier", id);
+
+                using NpgsqlDataReader reader = pgcom.ExecuteReader();
+                if (reader.Read())
+                {
+                    return getServiceResource(reader);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Authorization // ResourceRegistryRepository // DeleteResource // Exception");
+                throw;
+            }
+        }
+
+        public async Task<ServiceResource> GetResource(string id)
+        {
+            try
+            {
+                await using NpgsqlConnection conn = new NpgsqlConnection(_connectionString);
+                await conn.OpenAsync();
+
+                NpgsqlCommand pgcom = new NpgsqlCommand(getResource, conn);
                 pgcom.Parameters.AddWithValue("_identifier", id);
                 
                 using NpgsqlDataReader reader = pgcom.ExecuteReader();
@@ -67,11 +142,6 @@ namespace Altinn.ResourceRegistry.Persistence
             }
         }
 
-        Task IResourceRegistryRepository.UpdateResource(ServiceResource resource)
-        {
-            throw new NotImplementedException();
-        }
-
         private static ServiceResource getServiceResource(NpgsqlDataReader reader)
         {
             if (reader["serviceresourcejson"] != DBNull.Value)
@@ -82,6 +152,11 @@ namespace Altinn.ResourceRegistry.Persistence
                 return resource;
             }
             return null;
+        }
+
+        public Task<ServiceResource> UpdateResource(ServiceResource resource)
+        {
+            throw new NotImplementedException();
         }
     }
 }
