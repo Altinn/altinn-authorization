078ed07d99a25e43e91527e0d34dfc9cfc4359e6
Henning Normann <h.normann@accenture.com>
2022-11-02 10:46:09 +0100

    Removed logging

--- src/Authorization/Controllers/DecisionController.cs
+++ src/Authorization/Controllers/DecisionController.cs
@@ -205,11 +205,11 @@ namespace Altinn.Platform.Authorization.Controllers
         {
             decisionRequest = await this._contextHandler.Enrich(decisionRequest);
 
-            _logger.LogInformation($"// DecisionController // Authorize // Roles // Enriched request: {JsonConvert.SerializeObject(decisionRequest)}.");
+            ////_logger.LogInformation($"// DecisionController // Authorize // Roles // Enriched request: {JsonConvert.SerializeObject(decisionRequest)}.");
             XacmlPolicy policy = await this._prp.GetPolicyAsync(decisionRequest);
 
             XacmlContextResponse rolesContextResponse = _pdp.Authorize(decisionRequest, policy);
-            _logger.LogInformation($"// DecisionController // Authorize // Roles // XACML ContextResponse: {JsonConvert.SerializeObject(rolesContextResponse)}.");
+            ////_logger.LogInformation($"// DecisionController // Authorize // Roles // XACML ContextResponse: {JsonConvert.SerializeObject(rolesContextResponse)}.");
 
             XacmlContextResult roleResult = rolesContextResponse.Results.First();
             if (roleResult.Decision.Equals(XacmlContextDecision.NotApplicable))
