fa2bd8823eb1088cfd291cc52c83c18218a50838
Rune T. Larsen <rune@hjemmekino.no>
2022-11-24 13:01:01 +0100

    Added a couple of person tests

--- src/Altinn.Common.PEP/UnitTests/ResourceAccessHandlerTest.cs
+++ src/Altinn.Common.PEP/UnitTests/ResourceAccessHandlerTest.cs
@@ -97,6 +97,50 @@ namespace Altinn.Common.PEP.Authorization
             Assert.Equal("invalid party organization", exception.Message);
         }
 
+        /// <summary>
+        /// Test case: Send request and get response that fulfills all requirements
+        /// Expected: True
+        /// </summary>
+        [Fact]
+
+        public async Task HandleRequirementAsync_TC04Async()
+        {
+            // Arrange 
+            AuthorizationHandlerContext context = CreateAuthorizationHandlerContext();
+            _httpContextAccessorMock.Setup(h => h.HttpContext).Returns(CreateHttpContext("person", null, "01014922047"));
+            XacmlJsonResponse response = CreateResponse(XacmlContextDecision.Permit.ToString());
+            _pdpMock.Setup(a => a.GetDecisionForRequest(It.IsAny<XacmlJsonRequestRoot>())).Returns(Task.FromResult(response));
+
+            // Act
+            await _rah.HandleAsync(context);
+
+            // Assert
+            Assert.True(context.HasSucceeded);
+            Assert.False(context.HasFailed);
+        }
+
+        /// <summary>
+        /// Test case: Send request and get response that fulfills all requirements
+        /// Expected: Exception since format of who is incorrect
+        /// </summary>
+        [Fact]
+
+        public async Task HandleRequirementAsync_TC05Async()
+        {
+            // Arrange 
+            AuthorizationHandlerContext context = CreateAuthorizationHandlerContext();
+            _httpContextAccessorMock.Setup(h => h.HttpContext).Returns(CreateHttpContext("person", null, "a01014922047"));
+            XacmlJsonResponse response = CreateResponse(XacmlContextDecision.Permit.ToString());
+            _pdpMock.Setup(a => a.GetDecisionForRequest(It.IsAny<XacmlJsonRequestRoot>())).Returns(Task.FromResult(response));
+
+            // Act
+            Task Act() => _rah.HandleAsync(context);
+
+            // Assert
+            ArgumentException exception = await Assert.ThrowsAsync<ArgumentException>(Act);
+            Assert.Equal("invalid party person", exception.Message);
+        }
+
         private ClaimsPrincipal CreateUser()
         {
             // Create the user
