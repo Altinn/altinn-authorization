ad7ec18b328f89c17f78b4bf7c0cc61cdded27df
Terje Holene <terje.holene@gmail.com>
2023-04-19 13:18:52 +0200

    Feature/stop scope access handler from calling fail (#314)
    * Remove calls to context.Fail in ScopeAccessHandler
    
    * Update package version
    
    * Introduce IScopeAccessRequirement interface
    
    * Fix code smells

--- src/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/ScopeAccessHandler.cs
+++ src/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/ScopeAccessHandler.cs
@@ -1,43 +1,31 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Logging;
 
 namespace Altinn.Common.PEP.Authorization
 {
     /// <summary>
     /// Represents an authorization handler that can perform authorization based on scope
     /// </summary>
-    public class ScopeAccessHandler : AuthorizationHandler<ScopeAccessRequirement>
+    public class ScopeAccessHandler : AuthorizationHandler<IScopeAccessRequirement>
     {
-        private readonly ILogger _logger;
-        
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ScopeAccessHandler"/> class.
-        /// </summary>
-        /// <param name="logger">A logger from the logger factory</param>
-        public ScopeAccessHandler(ILogger<ScopeAccessHandler> logger)
-        {
-            _logger = logger;
-        }
-
         /// <summary>
         /// Performs necessary logic to evaluate the scope requirement.
         /// </summary>
         /// <param name="context">The current <see cref="AuthorizationHandlerContext"/></param>
         /// <param name="requirement">The scope requirement to evaluate.</param>
         /// <returns>Returns a Task for async await</returns>
-        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ScopeAccessRequirement requirement)
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, IScopeAccessRequirement requirement)
         {
             // get scope parameter from  user claims
-            string contextScope = context?.User?.Identities 
+            string contextScope = context.User?.Identities 
                 ?.FirstOrDefault(i => i.AuthenticationType != null && i.AuthenticationType.Equals("AuthenticationTypes.Federation"))?.Claims
                 .Where(c => c.Type.Equals("urn:altinn:scope"))?
                 .Select(c => c.Value).FirstOrDefault();
 
-            contextScope ??= context?.User?.Claims.Where(c => c.Type.Equals("scope")).Select(c => c.Value).FirstOrDefault();
+            contextScope ??= context.User?.Claims.Where(c => c.Type.Equals("scope")).Select(c => c.Value).FirstOrDefault();
 
             bool validScope = false;
 
@@ -45,7 +33,7 @@ namespace Altinn.Common.PEP.Authorization
             if (!string.IsNullOrWhiteSpace(contextScope))
             {
                 string[] requiredScopes = requirement.Scope;
-                List<string> clientScopes = contextScope?.Split(' ').ToList();
+                List<string> clientScopes = contextScope.Split(' ').ToList();
 
                 foreach (string requiredScope in requiredScopes)
                 {
@@ -60,10 +48,6 @@ namespace Altinn.Common.PEP.Authorization
             if (validScope)
             {
                 context.Succeed(requirement);
-            }
-            else
-            {
-                context.Fail();
             }
 
             await Task.CompletedTask;
