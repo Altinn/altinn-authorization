552b08866d6bc72df8ea15dfb28d33e58a1a5ade
Terje Holene <terje.holene@gmail.com>
2023-04-19 13:02:45 +0200

    Fix code smells

--- src/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/ScopeAccessRequirement.cs
+++ src/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/ScopeAccessRequirement.cs
@@ -9,7 +9,7 @@ namespace Altinn.Common.PEP.Authorization
     /// <see href="https://docs.asp.net/en/latest/security/authorization/policies.html"/> for details about authorization
     /// in asp.net core.
     /// </summary>
-    public class ScopeAccessRequirement : IAuthorizationRequirement, IScopeAccessRequirement
+    public class ScopeAccessRequirement : IScopeAccessRequirement
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ScopeAccessRequirement"/> class and 
