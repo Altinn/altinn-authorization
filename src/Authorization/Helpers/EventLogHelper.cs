8fa9459c0b82f07658f7b4a98f7cf6c8c6172e92
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-11-28 07:41:54 +0100

    Added forwarded headers middleware to map the client ip to the remoteip property of the httpcontext

--- src/Authorization/Helpers/EventLogHelper.cs
+++ src/Authorization/Helpers/EventLogHelper.cs
@@ -176,15 +176,8 @@ namespace Altinn.Platform.Authorization.Helpers
         /// <returns></returns>
         public static string GetClientIpAddress(HttpContext context)
         {
-            // Try to get the client IP address from the X-Real-IP header
-            var clientIp = context.Request.Headers["X-Real-IP"].FirstOrDefault();
-
-            // If the X-Real-IP header is not present, fall back to the RemoteIpAddress property
-            if (string.IsNullOrEmpty(clientIp))
-            {
-                clientIp = context.Connection.RemoteIpAddress?.ToString();
-            }
-
+            // The first ipaddress in the x-forwarded-for header is the client ipaddress. The ip from x-forwarded-for is read into remoteip depending on the forward limit
+            string clientIp = context?.Connection?.RemoteIpAddress?.ToString();
             return clientIp;
         }
     }
