4b5b438cc688bef205bf009e77f283c379e4756c
Terje Holene <terje.holene@gmail.com>
2024-01-11 14:46:10 +0100

    Change exception message

--- src/Altinn.Authorization.ABAC/Utils/XacmlJsonXmlConverter.cs
+++ src/Altinn.Authorization.ABAC/Utils/XacmlJsonXmlConverter.cs
@@ -219,7 +219,7 @@ namespace Altinn.Authorization.ABAC.Utils
                 case XacmlConstants.DataTypes.XMLInteger:
                     return XacmlConstants.DataTypes.XMLInteger;
                 default:
-                    throw new Exception("Not supported");
+                    throw new Exception($"Given data type; {jsonAttribute.DataType}, is not supported.");
             }
         }
     }
