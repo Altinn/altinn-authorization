b9a4815b48f8793613b262c2162d5fc16efde2e8
Andreas Isnes <andreas.isnes@gmail.com>
2024-07-15 17:15:51 +0200

    add vnet

--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -1,20 +1,24 @@
 locals {
-  size = 22
+  cidr_prefix        = tonumber(split("/", var.cidr)[1])
+  small_subnet_mask  = 28 - cidr_prefix # IPs 16
+  medium_subnet_mask = 26 - cidr_prefix # IPs 64
+  large_subnet_mask  = 24 - cidr_prefix # IPs 256
+
   subnets = {
     key_vault = {
-      address_space = 4
+      bits = local.medium_subnet_mask
     }
     app_configuration = {
-      address_space = 2
+      bits = local.medium_subnet_mask
     }
     storage_accounts = {
-      address_space = 3
+      bits = local.small_subnet_mask
     }
     redis = {
-      address_space = 4
+      bits = local.small_subnet_mask
     }
     postgres = {
-      address_space = 5
+      bits = local.small_subnet_mask
       delegations = {
         fs = {
           name = "Microsoft.DBforPostgreSQL/flexibleServers"
@@ -31,18 +35,27 @@ data "azurerm_resource_group" "vnet" {
   name = var.resource_group_name
 }
 
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.metadata.solution}-${var.metadata.enviroment}-${var.metadata.instance}"
-  address_space       = var.cidr
+  address_space       = [var.cidr]
   location            = data.azurerm_resource_group.vnet.location
   resource_group_name = data.azurerm_resource_group.vnet.name
 }
 
+module "subnet" {
+  source          = "hashicorp/subnets/cidr"
+  base_cidr_block = var.cidr
+  networks = [for key, value in local.subnets : {
+    name     = key
+    new_bits = value.subnet_mask
+  }]
+}
 
 resource "azurerm_subnet" "vnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ""
+  address_prefixes     = module.subnet[each.key]
   name                 = each.key
 
   dynamic "delegation" {
